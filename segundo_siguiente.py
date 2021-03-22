a9387b2508b27d9f56b24fb960766e31a4daf3f6
jzaldivar <jorge.zaldivar@gmail.com>
2021-03-22 01:43:19 -0600

    Primera propuesta de solución

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -2,14 +2,16 @@
 Inserta el encabezado aquí y escribe tu código abajo
 """
 
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+horas = int(input("Horas: "))
+minutos = int(input("Minutos: "))
+segundos = int(input("Segundos: "))
 
 # Proceso
-
+segundos += 1
 
 # Salidas
-print(salida)
+print("Horas:", horas)
+print("Minutos:", minutos)
+print("Segundos: ", segundos)
